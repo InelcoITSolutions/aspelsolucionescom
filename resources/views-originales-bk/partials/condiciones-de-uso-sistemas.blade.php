9bbae2cf725f3e21ea5ad789eb1627248e1e9348
Luis Carrillo <luisr_carrillo@outlook.com>
2018-07-13 15:55:57 -0500

    Se eliminaron los archivos innecesarios.

--- resources/views-originales-bk/partials/condiciones-de-uso-sistemas.blade.php
+++
@@ -1,3 +0,0 @@
-<p class="catpath">
-    Entre sus funciones están: compartir y controlar escritorios, reuniones en línea, videoconferencias y transferencia de archivos entre ordenadores. Existen versiones para los sistemas operativos Microsoft Windows, Mac OS X,1 2 Linux,3 iOS,4 y Android.5 También es posible el acceso a un equipo remoto mediante un navegador web.Aunque el principal cometido de la aplicación es el control remoto, también incluye funciones de trabajo en equipo y presentación.
-</p>
